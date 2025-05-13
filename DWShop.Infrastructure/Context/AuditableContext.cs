4808d5d34595eff92a0f875a843659f11f6b4f86
Yael Chavez <you400@gmail.com>
2025-05-12 20:20:14 -0600

    audit

--- DWShop.Infrastructure/Context/AuditableContext.cs
+++ DWShop.Infrastructure/Context/AuditableContext.cs
@@ -1,5 +1,6 @@
 ﻿using DWShop.Domain.Contracts;
 using DWShop.Domain.Entities;
+using DWShop.Domain.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,12 +8,12 @@ namespace DWShop.Infrastructure.Context
 {
     public class AuditableContext : IdentityDbContext
     {
-        public AuditableContext(DbContextOptions options): base(options)
+        public AuditableContext(DbContextOptions options) : base(options)
         {
-            
+
         }
         public DbSet<Catalog> Catalog { get; set; }
-
+        public DbSet<Audit> Audit { get; set; }
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             foreach (var item in ChangeTracker.Entries<IAuditableEntity>().ToList())
@@ -31,6 +32,79 @@ namespace DWShop.Infrastructure.Context
             }
 
             return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+        private List<AuditEntry> onBeforeSaveChanges(string userId)
+        {
+            ChangeTracker.DetectChanges();
+            var auditEntries = new List<AuditEntry>();
+
+            foreach (var entry in ChangeTracker.Entries())
+            {
+
+                if (entry.Entity is Audit
+                    || entry.State == EntityState.Detached
+                    || entry.State == EntityState.Unchanged
+                    )
+                {
+                    continue;
+                }
+
+                var auditEntry = new AuditEntry(entry)
+                {
+                    TableName = entry.Entity.GetType().Name,
+                    UserId = userId
+                };
+
+                auditEntries.Add(auditEntry);
+
+                foreach (var property in entry.Properties)
+                {
+                    if (property.IsTemporary)
+                    {
+                        auditEntry.TemporaryProperties.Add(property);
+                        continue;
+                    }
+
+                    var propertyName = property.Metadata.Name;
+
+                    if (property.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.KeyValues[propertyName] = property.CurrentValue!;
+                    }
+
+                    switch (entry.State)
+                    {
+                        case EntityState.Deleted:
+                            auditEntry.AuditType = AuditType.Delete;
+                            auditEntry.OldValues[propertyName] = property.OriginalValue!;
+                            break;
+                        case EntityState.Modified:
+                            if (property.IsModified && property.OriginalValue?.Equals(property.CurrentValue) == false)
+                            {
+                                auditEntry.ChangedColumns.Add(propertyName);
+                                auditEntry.AuditType = AuditType.Update;
+                                auditEntry.NewValues[propertyName] = property.CurrentValue;
+                                auditEntry.OldValues[propertyName] = property.OriginalValue;
+                            }
+                            break;
+                        case EntityState.Added:
+                            auditEntry.AuditType = AuditType.Create;
+                            auditEntry.NewValues[propertyName] = property.CurrentValue!;
+                            break;
+  
+                    }
+                }
+            }
+
+            foreach (var auditEntry in auditEntries.Where(x=> !x.HasTemporaryProperties))
+            {
+                Audit.Add(auditEntry.ToAudit());
+            }
+
+            return auditEntries.Where(x => !x.HasTemporaryProperties).ToList();
+           
         }
     }
 }
